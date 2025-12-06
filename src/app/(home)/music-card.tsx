bde0d574b72afbe135ebb4ee0f9fac26211dfca4
HongguoTang <146158692+Uoghluvm@users.noreply.github.com>
2025-12-06 14:26:12 +0800

    Update music-card.tsx

--- src/app/(home)/music-card.tsx
+++ src/app/(home)/music-card.tsx
@@ -1,34 +1,33 @@
 import Card from '@/components/card'  
 import { useCenterStore } from '@/hooks/use-center'  
+import { useConfigStore } from './stores/config-store'  
 import { CARD_SPACING } from '@/consts'  
-import { styles as hiCardStyles } from './hi-card'  
-import { styles as socialButtonsStyles } from './social-buttons'  
-  
-export const styles = {  
-  width: 400,  // 控制宽度  
-  height: 352, // Spotify iframe 的固定高度  
-  order: 5     // 控制渲染顺序  
-}  
+import { HomeDraggableLayer } from './home-draggable-layer'  
   
 export default function MusicCard({ url }: { url: string }) {  
-  const center = useCenterStore()  
-    
-  return (  
-    <Card  
-      order={styles.order}  
-      width={styles.width}  
-      height={styles.height}  
-      x={center.x + hiCardStyles.width / 2 - socialButtonsStyles.width}  
-      y={center.y + hiCardStyles.height / 2 + CARD_SPACING + socialButtonsStyles.height + CARD_SPACING}>  
-      <iframe  
-        style={{ borderRadius: 12 }}  
-        src={url.replace("open.spotify.com/playlist", "open.spotify.com/embed/playlist")}  
-        width="100%"  
-        height="100%"  
-        frameBorder="0"  
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"  
-        loading="lazy"  
-      />  
-    </Card>  
-  )  
+	const center = useCenterStore()  
+	const { cardStyles } = useConfigStore()  
+	const styles = cardStyles.musicCard  
+	const hiCardStyles = cardStyles.hiCard  
+	const clockCardStyles = cardStyles.clockCard  
+	const calendarCardStyles = cardStyles.calendarCard  
+  
+	const x = styles.offsetX !== null ? center.x + styles.offsetX : center.x + CARD_SPACING + hiCardStyles.width / 2 - styles.offset  
+	const y = styles.offsetY !== null ? center.y + styles.offsetY : center.y - clockCardStyles.offset + CARD_SPACING + calendarCardStyles.height + CARD_SPACING  
+  
+	return (  
+		<HomeDraggableLayer cardKey='musicCard' x={x} y={y} width={styles.width} height={styles.height}>  
+			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className='overflow-hidden'>  
+				<iframe  
+					style={{ borderRadius: 12 }}  
+					src={url.replace("open.spotify.com/playlist", "open.spotify.com/embed/playlist")}  
+					width="100%"  
+					height="100%"  
+					frameBorder="0"  
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"  
+					loading="lazy"  
+				/>  
+			</Card>  
+		</HomeDraggableLayer>  
+	)  
 }
